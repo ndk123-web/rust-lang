4f17657926c4b061f4c4a60088e0601596116b6b
Navnath Kadam <navnathkadam284@gmail.com>
2026-07-03 21:02:48 +0530

    docs: added better docs for std mutex and tokio mutex

--- async_await/src/mutex.rs
+++ async_await/src/mutex.rs
@@ -2,7 +2,36 @@
  *
  * Arc support multiple owners
  * Mutex supports one writer at a time
- */
+ *
+ * We have 2 version of Mutex
+ * 1. std::sync::Mutex
+    - Assume 1 Tokio worker thread.
+    - Task A acquires the mutex.
+    - Task B tries to acquire the same mutex using lock().
+    - The worker thread gets BLOCKED.
+    - Because the thread is blocked, Task C (even if it doesn't need the mutex)
+    also cannot run.
+
+    Result:
+        A -> holds lock
+        B -> blocks the thread
+        C -> cannot execute
+
+
+ * 2. tokio::sync::Mutex
+
+    - Assume 1 Tokio worker thread.
+    - Task A acquires the mutex.
+    - Task B tries to acquire the same mutex using lock().await.
+    - Task B is suspended (not the thread).
+    - Tokio scheduler can now run Task C if it is ready.
+
+    Result:
+        A -> holds lock
+        B -> waiting for mutex
+        C -> continues executing
+*/
+
 use std::sync::{Arc, Mutex};
 use std::thread;
 
